8055cd36e509eab47bbfec84541196d0cd74f974
Keerthidhasarathan7979 <121286975+Keerthidhasarathan7979@users.noreply.github.com>
2023-08-26 13:31:56 +0530

    Add files via upload

---
+++ Student.cs
@@ -0,0 +1,13 @@
+﻿namespace nakuriproject
+{
+    public class Student
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+        public string? Name { get; set; }
+        public string? Email { get; set; }
+        public int Pass { get; set; }
+        public int Phone { get; set; }
+    }
+}
